c0507dc2b1e6e881990e485aa03e785044adcf3b
Collin Driscoll <provinite@gmail.com>
2021-10-09 23:56:16 -0500

    initial commit

--- lib/halloween-discord-stack.ts
+++ lib/halloween-discord-stack.ts
@@ -4,6 +4,5 @@ export class HalloweenDiscordStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
   }
 }
